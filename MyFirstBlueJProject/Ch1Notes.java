84d72603fc54fa5b52155c252a3168f1287bf731
MaeBinkowski <111481066+MaeBinkowski@users.noreply.github.com>
2022-08-23 14:20:21 -0500

    New blueJ file

---
+++ MyFirstBlueJProject/Ch1Notes.java
@@ -0,0 +1,38 @@
+/**
+ * Mae Binkowski
+ * 8/23/22
+ * Chapter 1 notes
+ * Notes from class
+ * Who helped me:
+ */
+
+import java.util.Scanner; // class is last arg
+import java.util.Random;
+
+public class Ch1Notes
+{
+    /**
+     * this is code notes for ch 1
+     */
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner (System.in);
+        
+        // basic data type
+        int x = 9, y, z;
+        boolean choice = false;
+        char letter = 'b';
+        float number2 = 9;
+        double number1 = 8.9, number3, number4;
+        short num = 6;
+        long numbbbber = 9999;
+        
+        // these are objects
+        // Class id      memory   Constructor 
+        String   word =  new      String("Mae Binkowski");
+        Integer  number= new      Integer(47);
+        Double   anumber=new      Double (45.8);
+        
+        System.out.println ("Mae Binkowski");
+    }
+}
